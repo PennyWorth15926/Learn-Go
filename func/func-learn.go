9c34fd678e598addbd9f930d812862fb079c5576
PennyWorth15926 <60127394+PennyWorth15926@users.noreply.github.com>
2022-09-06 19:38:20 +0800

    Create func-learn.go

---
+++ func/func-learn.go
@@ -0,0 +1,39 @@
+package main
+ 
+import (
+    "fmt"
+)
+
+type(    
+
+    People struct {
+        name  string
+        goal *Goal
+    }
+
+    Goal struct{
+        name string
+        Content string
+    }
+)
+
+func Changes(whisper *Goal, msg []byte) (*People, error) {
+    person := new(People)
+    person.goal = whisper
+    println(msg)
+    fmt.Println(msg)
+    return person, nil
+}  
+
+func main() {
+
+    whisper := &Goal{Content:"sleep a little"}
+    bytemsg := []byte("Hl Asong!")
+    person,err := Changes(whisper,bytemsg)
+    if err != nil {
+        fmt.Println("error message", err)
+    }
+    fmt.Println(person)
+    fmt.Println(person.goal)
+
+}
